a2d8014fd6a4c3db02e84bfa9a94a999554f3d91
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-11-20 21:18:55 +0530

    Add files via upload

---
+++ Decision_Making_Statment/Example_04.py
@@ -0,0 +1,15 @@
+#if you kip it if statment is blank then you can use pass,none,Ellipsis keyword.
+
+num=int(input("Enter a number :"))
+if(num > 20):
+    pass
+
+#none
+num=int(input("Enter number :"))
+if(num < 20):
+    None
+
+#Ellipsis
+num=int(input("Enter number :"))
+if(num < 20):
+    ...
