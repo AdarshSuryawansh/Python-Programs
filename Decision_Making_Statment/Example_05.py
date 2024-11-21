ea01734266943aaf99fe76b95124c01edfbe9318
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-11-21 08:58:22 +0530

    Add files via upload

---
+++ Decision_Making_Statment/Example_05.py
@@ -0,0 +1,9 @@
+ #Accept a charactor  from user and check weather it is vowel or consonant.
+
+
+
+ch = input("Plase enter a single charactor :")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("It is vowel")
+else:
+    print("Is is consonant")
