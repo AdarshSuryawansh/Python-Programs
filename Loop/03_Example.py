6e23f027d256d8ab6ca18be9cfc82ba605083ac5
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-11-23 15:34:28 +0530

    Add files via upload

---
+++ Loop/03_Example.py
@@ -0,0 +1,8 @@
+#check all element from list whether it is even or odd
+
+list1=[12,23,34,45,56,67,78,89,90,18]
+for i in list1:
+    if(i % 2 == 0):
+        print(i,"Even Number")
+    else:
+        print(i,"Odd Number")
