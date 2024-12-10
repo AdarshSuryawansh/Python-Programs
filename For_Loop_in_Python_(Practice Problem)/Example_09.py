2c2a3f583d55728a2874903038846ef1497e7cec
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-12-10 10:25:09 +0530

    Add files via upload

---
+++ For_Loop_in_Python_(Practice Problem)/Example_09.py
@@ -0,0 +1,10 @@
+
+#Example 9: Python program that accepts a word from the user and reverses it.
+
+usr_str = input("Enter a some String :")
+reverse_str = ""
+
+for i in usr_str:
+    reverse_str = i + reverse_str
+
+print(reverse_str)
