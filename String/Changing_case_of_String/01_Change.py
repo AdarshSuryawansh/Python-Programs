6e24a0c7d9205cf3e5f6d997480b6ca1fe0a9ea4
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-12-18 09:40:40 +0530

    Add files via upload

---
+++ String/Changing_case_of_String/01_Change.py
@@ -0,0 +1,11 @@
+#Changing to the case of string
+# upper()
+# lower()
+# title()
+# capitalize()
+
+s = "Learning pyrhon is very easy"
+print(s.upper())
+print(s.lower())
+print(s.title())
+print(s.capitalize())
