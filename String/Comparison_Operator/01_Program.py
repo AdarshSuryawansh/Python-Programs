d9978960a7f3c0752d258e8f4fcb6100399eab77
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-12-19 17:55:20 +0530

    Add files via upload

---
+++ String/Comparison_Operator/01_Program.py
@@ -0,0 +1,11 @@
+#Q.Get two string  from users and to check equal or not
+# aski cods use to find greater than or less than.
+
+s1 = (input("Enter a some string for s1 :"))
+s2 = (input("Enter a some string for s2 :"))
+if(s1 > s2):
+    print("s1 greater than s2")
+elif(s2 > s1):
+    print("s2 greater than s1")
+else:
+    print("Both are equal !!!")
