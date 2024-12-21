d553215a85ea37695f44c4fd82df60e24f8f5a21
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-12-21 11:11:47 +0530

    Add files via upload

---
+++ String/Counting_the_sub-string_in_main-string/01_Count.py
@@ -0,0 +1,5 @@
+#Counting the sub-string in main-string
+#count() ==>count the number of occurances  of sub-string in main string
+
+var = "abcabcabcabcabc"
+var.count("a")
---
+++ String/Counting_the_sub-string_in_main-string/02_Count.py
@@ -0,0 +1,4 @@
+#count("sub-string",start_index,end_index")
+
+var = "abcabcabcabcabcabc"
+var.count("a",3,7)
