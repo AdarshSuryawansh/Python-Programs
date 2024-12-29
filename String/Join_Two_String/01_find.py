52e3f662fe552daf3f63367931ac37b54acef4c1
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2024-12-29 11:26:12 +0530

    Add files via upload

---
+++ String/Join_Two_String/01_find.py
@@ -0,0 +1,6 @@
+#Finding the sub-string.
+#find() ==> It will returns the starting position of substring from the left hand side.
+#rfind() ==> It will returns the starting position of sub-string from the right hand side.
+
+str_ = "Learning Python is very easy"
+str_.find("Python")
