9e58cee585981414be342b613b7680e7b45b725f
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2025-01-02 12:31:40 +0530

    Add files via upload

---
+++ String/Membership_Operator/in_Operator.py
@@ -0,0 +1,6 @@
+# in opeartor
+# check the presence of value in any iteriable object.
+
+a = "Python"
+b = "Python"
+print("p" in a)
---
+++ String/Membership_Operator/is_operator.py
@@ -0,0 +1,7 @@
+#Membership Operator
+# is opeartor
+# checking the referance comparasion
+
+a = 10
+b = 10
+print(a is b)
