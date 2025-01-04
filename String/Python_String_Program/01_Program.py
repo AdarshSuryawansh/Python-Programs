8cd7ce5d62849e898e21d45058113f0ec02e65b8
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2025-01-04 11:31:34 +0530

    Add files via upload

---
+++ String/Python_String_Program/01_Program.py
@@ -0,0 +1,8 @@
+#Q1.Take Two String from user and chaeck if they are equal or not.
+
+s1 = input("Enter some string :").lower()
+s2 = input("Enter some string :").lower()
+if s1 == s2:
+    print("Both are equal")
+else:
+    print("Both are different")
