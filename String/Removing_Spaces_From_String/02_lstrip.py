bc0bd8c69bb4aea464ec11ae63f57e769cdf4864
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2025-01-11 13:18:51 +0530

    Add files via upload

---
+++ String/Removing_Spaces_From_String/02_lstrip.py
@@ -0,0 +1,7 @@
+#lstrip() ==> removes the spaces from the left hand side.
+#lstrip
+str = (input("Enter some string :")).lstrip()
+if str == "sunny":
+    print("Valid")
+else:
+    print("Invalid !!!")
