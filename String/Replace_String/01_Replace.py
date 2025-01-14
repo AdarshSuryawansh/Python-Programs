33ffd3be978c523becfdbfc0e5db348b5a4518d5
Adarsh Suryawanshi <103188189+AdarshSuryawansh@users.noreply.github.com>
2025-01-14 10:52:18 +0530

    Add files via upload

---
+++ String/Replace_String/01_Replace.py
@@ -0,0 +1,7 @@
+#Replae the string with another string
+#replace("old_string","new_string") ==>replace old string with new string.
+
+var = "abcabcabcabcabcabcacabcbac"
+var = "Learning Python is vary easy"
+var.replace("a","A")
+var.replace("Python","Power BI")
