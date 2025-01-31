e875477230618f5c8b1df1d80708420d9e6843cb
Adarsh Suryawanshi <adarshsuryawanshi9992@gmail.com>
2025-01-31 17:46:49 +0530

    Add files via upload

---
+++ Python_Coding_Interview_Question/19_Question.py
@@ -0,0 +1,9 @@
+
+# Adding Two list Element Togehter
+
+lst1 = [1,2,3,4]
+lst2 = [5,6,7,8]
+res_lst = []
+for i in range(0,len(lst1)):
+    res_lst.append(lst1[i] + lst2[i])
+print(res_lst)
---
+++ Python_Coding_Interview_Question/21_Question.py
@@ -0,0 +1,10 @@
+
+# Chacking for Palindrom using Extended slicing method
+
+str1 = "madam".lower()
+str2 = "madam".lower()
+
+if(str1 == str2[::-1]):
+    print("True")
+else:
+    print("False")
